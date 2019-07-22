f78bad2bd4974c1e87e2e94c8d62962abc3a6fe4
Micleyu <1635872541@qq.com>
2019-07-22 17:28:13 +0800

    吸顶效果

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 
-// 锚点
+//导航固定
+function navFixed() {
+    var nav = document.getElementById('headerban-nav');
+    var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scrolltop > 495 || scrolltop==495) {
+        nav.style.position = "fixed";
+        nav.style.top = "0";
+    }else{
+        nav.style.position = "static";
+    }
+}
+
+window.onscroll = function(){
+    navFixed()
+};
+
+// 
 $('.headerban-nav-left li a').on('click',function(){
     var mapId=$(this).attr('data-map');
     var mapTop=$('.'+mapId).offset().top-45;
